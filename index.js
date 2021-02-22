eacb378812f6e4e50a76d50ce6f0274addc3e9a1
ccchippp <clempke90@gmail.com>
2021-02-22 16:46:09 -0500

    commit tree 43349868cf945166add4218a366bd34ce599129e

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Your code here
+
+function saturdayFun(activity="roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+
+function mondayWork(activity="go to the office") {
+    return (`This Monday, I will ${activity}.`)
+}
+mondayWork()
+
+function wrapAdjective(wrapper="*") {
+    return function(special="special") {
+        return `You are ${wrapper}${special}${wrapper}!`
+    }
+}
+
+const Calculator = {
+    add: function(num1, num2){
+    return num1 + num2;
+    },
+    subtract: function(num1, num2){
+        return num1 - num2;
+    },
+    multiply: function(num1, num2){
+        return num1 * num2
+    },
+    divide: function(num1, num2){
+        return num1 / num2
+    }
+}
+
+function actionApplyer(start, array) {
+    let num = start
+    for (let a = 0; a < array.length; a++ ){
+    num = array[a](num)
+    }
+    return num
+}
